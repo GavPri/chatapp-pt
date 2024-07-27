e34f52d1e6f2f65a2467366bd19f8ee5e5779257
德华 刘 <gavinprior5@gmail.com>
2024-07-27 13:26:11 +0000

    create sidebar component for Channel list conatiner

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,9 +3,29 @@ import React from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 // ----- cookie import
 import Cookies from "universal-cookie";
+// ---- Icon imports
+import { GiBiceps } from "react-icons/gi";
+import { PiSignOutLight } from "react-icons/pi";
+
+// ---- Sidebar
+const SideBar = () => (
+  <div className="w-2/12 bg-slate-300">
+    {/* Icon */}
+    <div className="group p-4 flex justify-center items-center text-slate-100 bg-slate-900 rounded-lg w-8/12 mx-auto my-6 transition-all duration-500 hover:cursor-pointer hover:bg-violet-600 hover:text-violet-100">
+      <GiBiceps size={50} className="group-hover:scale-105" />
+    </div>
+    <div className="group p-4 flex justify-center items-center text-slate-100 bg-slate-900 rounded-lg w-8/12 mx-auto my-6 transition-all duration-500 hover:cursor-pointer hover:bg-violet-600 hover:text-violet-100">
+      <PiSignOutLight size={50} className="group-hover:scale-105" />
+    </div>
+  </div>
+);
 
 const ChannelListContainer = () => {
-  return <div>ChannelListContainer</div>;
+  return (
+    <>
+      <SideBar />
+    </>
+  );
 };
 
 export default ChannelListContainer;
