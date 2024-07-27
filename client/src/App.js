775b406e77c7fea664b8b3d0376031385a8535aa
德华 刘 <gavinprior5@gmail.com>
2024-07-27 12:13:40 +0000

    create variable for api key

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
+// ----- API key 
+const apiKey = "7dcuwnvqfkfp";
+
 const App = () => {
   return <div>Hybrid Training</div>;
 };
