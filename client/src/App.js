807f9d8bebd4979cbb50bb352bc4ee1cda494783
德华 刘 <gavinprior5@gmail.com>
2024-07-27 12:21:10 +0000

    install and cofigure tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,11 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-// ----- API key 
+// ----- API key
 const apiKey = "7dcuwnvqfkfp";
 
 const App = () => {
-  return <div>Hybrid Training</div>;
+  return <div className="text-3xl text-slate-500">Hybrid Training</div>;
 };
 
 export default App;
