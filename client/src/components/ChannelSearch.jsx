c99ac0b89f6b52c0fd65e7bc2a12fd5b1c822750
德华 刘 <gavinprior5@gmail.com>
2024-07-27 13:59:27 +0000

    add boilerplate style to channel search

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
+import { IoIosSearch } from "react-icons/io";
 
 const ChannelSearch = () => {
-  return <div>ChannelSearch</div>;
+  return (
+    // ---- Component wrapper
+    <div className="relative flex justify-center items-center pt-[16px]">
+      {/* ----- Search input wrapper */}
+      <div className="flex justify-center items-center h-40 bg- bg-slate-100 rounded-md border-2-transparent width w-10/12">
+        {/* Icon  */}
+        <div className="flex justify-center">
+          <IoIosSearch />
+        </div>
+        <input
+          type="text"
+          value={query}
+          onChange={onUserSearch}
+          placeholder="Search"
+          className="px-4 py2"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ChannelSearch;
