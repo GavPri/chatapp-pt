be8d8baa83f66edb21f9bba0b0d077d145393a52
德华 刘 <gavinprior5@gmail.com>
2024-07-27 12:51:14 +0000

    import cookies, ChannelList and use chat context

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+// ---- stream chat react imports
+import { ChannelList, useChatContext } from "stream-chat-react";
+// ----- cookie import
+import Cookies from "universal-cookie";
 
 const ChannelListContainer = () => {
-  return (
-    <div>ChannelListContainer</div>
-  )
-}
+  return <div>ChannelListContainer</div>;
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
