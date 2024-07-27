ff0f8ccafa197753dd7cab1144c6bc3e680f33f0
德华 刘 <gavinprior5@gmail.com>
2024-07-27 15:51:51 +0000

    install and configure shadcn

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Terminal } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "src/components/ui/alert";
 
 const TeamChannelList = ({ childer, error = false, loading, type }) => {
-  return <div></div>;
+  return (
+    <>
+    </>
+  );
 };
 
 export default TeamChannelList;
