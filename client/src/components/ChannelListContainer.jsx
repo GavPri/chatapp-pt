f3bad33c62732b6f198a6e86b6c12b10b4fa4ccc
德华 刘 <gavinprior5@gmail.com>
2024-07-27 13:40:06 +0000

    create and render company header in channel list container

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -9,7 +9,7 @@ import { PiSignOutLight } from "react-icons/pi";
 
 // ---- Sidebar
 const SideBar = () => (
-  <div className="w-2/12 bg-slate-300">
+  <div className="w-1/12 bg-slate-300">
     {/* Icon */}
     <div className="group p-4 flex justify-center items-center text-slate-100 bg-slate-900 rounded-lg w-8/12 mx-auto my-6 transition-all duration-500 hover:cursor-pointer hover:bg-violet-600 hover:text-violet-100">
       <GiBiceps size={50} className="group-hover:scale-105" />
@@ -20,10 +20,19 @@ const SideBar = () => (
   </div>
 );
 
+const CompanyHeader = () => (
+  <div className="w-full h-24 mt-6">
+    <p className="mx-auto text-center font-bold">Hybrid Training Center</p>
+  </div>
+);
+
 const ChannelListContainer = () => {
   return (
     <>
       <SideBar />
+      <div className="flex flex-col bg-slate-400 w-2/12">
+        <CompanyHeader />
+      </div>
     </>
   );
 };
