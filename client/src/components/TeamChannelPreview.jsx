2072aea9fe6f91b7fdfe992d8c6f035891452271
德华 刘 <gavinprior5@gmail.com>
2024-08-02 16:12:36 +0000

    create hook useChatContext

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
 
 const TeamChannelPreview = ({ children, type }) => {
+  // useChatContectHook
+  const { channel, client } = useChatContext();
+
   return <div>TeamChannelPreview</div>;
 };
 
