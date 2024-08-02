2198fe7584cd9d794c6a7a6ab4ef13c2ce179c83
德华 刘 <gavinprior5@gmail.com>
2024-08-02 15:19:01 +0000

    create error alert in search TeamChannelList
    Use alert from shadcn ui, installed in cli, correct typo in children prop

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Terminal } from "lucide-react";
+import { AlertCircle } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "src/components/ui/alert";
 
-const TeamChannelList = ({ childer, error = false, loading, type }) => {
-  return (
-    <>
-    </>
-  );
+const TeamChannelList = ({ children, error = true, loading, type }) => {
+  // set state for error message
+  if (error) {
+    return type === "team" ? (
+      <Alert
+        variant="destructive"
+        className="h-24 w-10/12 mx-auto mt-4 bg-slate-950"
+      >
+        <AlertCircle className="h-6 w-6" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>Connection error. Please try again</AlertDescription>
+      </Alert>
+    ) : null;
+  }
+  return <></>;
 };
 
 export default TeamChannelList;
