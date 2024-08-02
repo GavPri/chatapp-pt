16972fe9c57ff12c8187cbce2db313c2ed35e084
德华 刘 <gavinprior5@gmail.com>
2024-08-02 16:54:41 +0000

    install axios. import useState, cookies & axios

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  return <div>Auth</div>;
+};
 
-export default Auth
+export default Auth;
