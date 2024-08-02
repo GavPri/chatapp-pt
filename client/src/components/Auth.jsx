ba76449d5d0211a39403d5e5d32626e959fd7c9c
德华 刘 <gavinprior5@gmail.com>
2024-08-02 17:06:47 +0000

    add basic styles to container and form container

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,7 +3,12 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 
 const Auth = () => {
-  return <div className="h-screen w-screen bg-slate-100"></div>;
+  return (
+    <div className="h-screen w-screen bg-slate-100 flex justify-center items-center">
+      {/* Form Container */}
+      <div className="w-3/4 h-3/4 bg-slate-200 border-2 border-violet-300 rounded-md drop-shadow-md shadow-violet-400"></div>
+    </div>
+  );
 };
 
 export default Auth;
