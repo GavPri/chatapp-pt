b98fbf198b808f2f850d7cfe29b6ccd35a683c84
德华 刘 <gavinprior5@gmail.com>
2024-08-02 17:17:40 +0000

    create state to handle if sign in for or sign up form

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,10 +3,30 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleIsSignUp = () => {
+    setIsSignUp(!isSignUp);
+  };
   return (
     <div className="h-screen w-screen bg-slate-100 flex justify-center items-center">
       {/* Form Container */}
-      <div className="w-3/4 h-3/4 bg-slate-200 border-2 border-violet-300 rounded-md drop-shadow-md shadow-violet-400"></div>
+      <div className="w-3/4 h-3/4 bg-slate-200 border-2 border-violet-300 rounded-md drop-shadow-md shadow-violet-400">
+        {/* Form Header */}
+        <h1 className="text-3xl text-violet-500 text-center mt-4">
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </h1>
+        {/* Change form button */}
+        <p className="text-center">
+          Already got an account?{" "}
+          <span
+            onClick={handleIsSignUp}
+            className="hover:cursor-pointer text-violet-400 hover:text-violet-500"
+          >
+            Click here
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
