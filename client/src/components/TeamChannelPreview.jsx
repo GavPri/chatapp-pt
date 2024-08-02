b3469b83515747f917204c2ffbcc1a704515db82
德华 刘 <gavinprior5@gmail.com>
2024-08-02 16:11:05 +0000

    import avatar and useChatContect to TeamChannelPreview

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
 
-const TeamChannelPreview = ({children, type}) => {
-  return (
-    <div>TeamChannelPreview</div>
-  )
-}
+const TeamChannelPreview = ({ children, type }) => {
+  return <div>TeamChannelPreview</div>;
+};
 
-export default TeamChannelPreview
+export default TeamChannelPreview;
