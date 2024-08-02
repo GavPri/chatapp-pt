f9eb35566c6571b6bb3e3075ad778040719fb5e2
德华 刘 <gavinprior5@gmail.com>
2024-08-02 15:43:54 +0000

    add type prop to channellist in channellistcontainer

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -38,7 +38,8 @@ const ChannelListContainer = () => {
         <ChannelList
           filters={{}}
           channelRenderFilterFn={() => {}}
-          List={(listProps) => <TeamChannelList {...listProps} />}
+          List={(listProps) => <TeamChannelList {...listProps}
+          type='team' />}
         />
       </div>
     </>
