61f28ef1dbab0325e623ef059a70fa6431496129
德华 刘 <gavinprior5@gmail.com>
2024-08-12 19:30:01 +0000

    create initial file upload to accept images

--- client/src/components/AuthComponents/FormInputs.jsx
+++ client/src/components/AuthComponents/FormInputs.jsx
@@ -43,6 +43,21 @@ const FormInputs = ({ handleIsSignUp, isSignUp }) => {
           />
         </>
       )}
+      {/* TODO change from default file input */}
+      {isSignUp && (
+        <>
+          <label htmlFor="avatarURL" className="sr-only">
+            Avatar URL
+          </label>{" "}
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            placeholder="Avatar"
+            name="avatarURL"
+            className="w-full rounded-md drop-shadow-sm my-4 py-4 px-4"
+          />
+        </>
+      )}
     </div>
   );
 };
