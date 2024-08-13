a47e90f0824cf4ff5d113d8dfe43769a866b8614
德华 刘 <gavinprior5@gmail.com>
2024-08-13 12:51:16 +0000

    create boilerplate signup and login functions
    export them from auth.js

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+// * ----- Auth Login & Sign Up Controllers
+//  * Arrow functions for each 
+
+const login = () => {};
+const signup = () => {};
+
+
+// * Exporting the contollers as an object
+modules.exports = { signup , login}
