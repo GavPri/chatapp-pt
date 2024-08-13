be771aae9987a21506ffa60f7b213f77f09d563e
德华 刘 <gavinprior5@gmail.com>
2024-08-13 13:26:02 +0000

    create boilerplate controllers for login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,24 @@
 // * ----- Auth Login & Sign Up Controllers
 //  * Arrow functions for each 
 
-const login = () => {};
-const signup = () => {};
+const signup = (req, res) => {
+    // * try catch block 
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error})
+    }
+};
+const login = () => {
+  // * try catch block
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+};
+
 
 
 // * Exporting the contollers as an object
