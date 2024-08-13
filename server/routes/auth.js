2a05ddb9e9c494ac2cdbb819407ebcf14226447e
德华 刘 <gavinprior5@gmail.com>
2024-08-13 12:53:35 +0000

    import signup and login to auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,10 @@ const express = require("express");
 // * Get a router from that express instance
 const router = express.Router()
 
+// * Importing the controllers 
+const { signup, login } = require('../controllers/auth.js')
+
 // * All of the authentication routes will be post routes. We are sending data from the front end to the back end. 
+// ? We want functions to run on each call, these are the controllers.
 router.post('/signup')
 router.post('/login')
