ff1ff7612d4d65b74e9e78f45ea0861c933e8eee
德华 刘 <gavinprior5@gmail.com>
2024-08-13 12:27:35 +0000

    create app middleware

--- server/index.js
+++ server/index.js
@@ -12,3 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 // *Using dotenv to call enviorment variables.
 require("dotenv").config();
+
+// * Setting up the apps middleware
+app.use(cors()); // ? Allows us to use cross origin requests. 
+app.use(express.json()) // ? Allows us to use json payloads from the front end to the back end.
+app.use(express.urlencoded()) // ? Built in middleware with express.js
