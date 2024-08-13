4f4c9db1fa9b1a8c02b6b7aebf5e1863d4e24ef1
德华 刘 <gavinprior5@gmail.com>
2024-08-13 12:34:58 +0000

    create boilerplate server

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 // * Setting up the apps middleware
-app.use(cors()); // ? Allows us to use cross origin requests. 
-app.use(express.json()) // ? Allows us to use json payloads from the front end to the back end.
-app.use(express.urlencoded()) // ? Built in middleware with express.js
+app.use(cors()); // ? Allows us to use cross origin requests.
+app.use(express.json()); // ? Allows us to use json payloads from the front end to the back end.
+app.use(express.urlencoded()); // ? Built in middleware with express.js
+
+// * ------  App routes -------
+
+// ? Home route takes a url '/' , inthis case, create a call back function with req and res as parameters
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// * Check to make sure the server is running
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
