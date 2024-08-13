40515bca241324c258d5bbb13a8e5650119d9691
德华 刘 <gavinprior5@gmail.com>
2024-08-13 13:29:33 +0000

    destructure req.body to reuturn full name, username and password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,13 +4,15 @@
 const signup = (req, res) => {
     // * try catch block 
     try {
-        
+        // ? What do we need to get from the front end? We can destructure it as we get everything from the req.body.
+        const {fullName, username, password, phoneNumber} = req.body
+
     } catch (error) {
         console.log(error);
         res.status(500).json({message: error})
     }
 };
-const login = () => {
+const login = (req, res) => {
   // * try catch block
   try {
   } catch (error) {
